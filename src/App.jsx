7ce3e6f76e9a7721a52e6aef657b4b8f75f48cfa
shubhibajpai <bajpaishubhi51@gmail.com>
2026-05-01 22:58:27 +0530

    fix:path

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './Pages/Home/home';
 import Navbar from './Pages/Navbar';
 import About from './Pages/About/about';
 import Skills from './Pages/Skills/skills';
-import Projects from './Pages/Projects/projects';
+import Projects from './Pages/Projects/Projects';
 import ParticlesBg from './Component/ParticlesBg/ParticlesBg';
 function App() {
 
